f8f790529fa429d0a665696b24ee4f1666152675
NikhilJi143 <137680022+NikhilJi143@users.noreply.github.com>
2026-08-08 15:39:16 +0530

    fix cors for deployment

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,13 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 // Routes
